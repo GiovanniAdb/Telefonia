dcb9fbf9c573f4ea700f9f211d26219f2a92d333
GiovanniAdb <78819879+GiovanniAdb@users.noreply.github.com>
2023-03-08 17:44:58 -0600

    Funciona ya el BroadcastReceiver

--- app/src/main/java/com/example/telefonia/MainActivity.kt
+++ app/src/main/java/com/example/telefonia/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.telefonia
+import android.Manifest
 import android.content.*
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.telephony.SmsManager
 import android.util.Log
@@ -7,6 +9,9 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var targetNumber: EditText
@@ -19,8 +24,55 @@ class MainActivity : AppCompatActivity() {
         targetNumber = findViewById(R.id.editTextPhone)
         autoResponse = findViewById(R.id.editTextMessage)
 
+        val saveConfigButton = findViewById<Button>(R.id.buttonSave)
+        saveConfigButton.setOnClickListener {
+            // Obtener el número de teléfono y el mensaje de respuesta automáticos ingresados por el usuario
+            // Guardarlos en las preferencias compartidas usando la clase SharedPreferences
+            val sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE)
+            val editor = sharedPreferences.edit()
+            editor.putString("targetNumber", targetNumber.text.toString())
+            editor.putString("responseMessage", autoResponse.text.toString())
+            editor.apply()
+            Toast.makeText(this, "¡La configuracion se ha guardado!", Toast.LENGTH_SHORT).show();
+        }
+
+        val startServiceButton = findViewById<Button>(R.id.buttonStart)
+        startServiceButton.setOnClickListener {
+            // Iniciar el servicio MyService usando la función startService()
+            val intent = Intent(this, MyService::class.java)
+            startService(intent)
+            Toast.makeText(this, "¡El servicio se ha iniciado!", Toast.LENGTH_SHORT).show();
+        }
+
+        val stopServiceButton = findViewById<Button>(R.id.buttonStop)
+        stopServiceButton.setOnClickListener {
+            // Detener el servicio MyService usando la función stopService()
+            val intent = Intent(this, MyService::class.java)
+            stopService(intent)
+            Toast.makeText(this, "¡El servicio se ha detenido!", Toast.LENGTH_SHORT).show();
+
+            /**Era para probar que si llegaran los datos bien
+            val smsManager = SmsManager.getDefault()
+            smsManager.sendTextMessage("+52" + targetNumber.text.toString(), null, autoResponse.text.toString(), null, null)
+             */
+
+
+            /**
+            Toast.makeText(this, targetNumber.text.toString(), Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "---" + autoResponse.text.toString(), Toast.LENGTH_SHORT).show();
+            */
+        }
+    }
+}
+        /** Obtener permisos
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE)!=
+            PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_PHONE_STATE),369)
+        }
+
+
         // Cargar las preferencias guardadas
-        val prefs = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+        val prefs = getSharedPreferences("MyPrefs", MODE_PRIVATE)
         val editor = prefs.edit()
         editor.clear()
         editor.apply()
@@ -29,43 +81,7 @@ class MainActivity : AppCompatActivity() {
 
         Toast.makeText(this, "Num: "+targetNumber.text.toString(), Toast.LENGTH_SHORT).show();
         Toast.makeText(this, "Text: "+autoResponse.text.toString(), Toast.LENGTH_SHORT).show();
-        Log.d("esto es el mensaje de texto que se manda", autoResponse.toString())
+        //Log.d("esto es el mensaje de texto que se manda", autoResponse.toString())
         //targetNumber.setText("4171400040")
         //autoResponse.setText("que onda")
-
-    val saveConfigButton = findViewById<Button>(R.id.buttonSave)
-    saveConfigButton.setOnClickListener {
-        // Obtener el número de teléfono y el mensaje de respuesta automáticos ingresados por el usuario
-        // Guardarlos en las preferencias compartidas usando la clase SharedPreferences
-        val sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
-        val editor = sharedPreferences.edit()
-        editor.putString("targetNumber", targetNumber.text.toString())
-        editor.putString("responseMessage", autoResponse.text.toString())
-        editor.apply()
-        Toast.makeText(this, "¡La configuracion se ha guardado!", Toast.LENGTH_SHORT).show();
-    }
-
-    val startServiceButton = findViewById<Button>(R.id.buttonStart)
-    startServiceButton.setOnClickListener {
-        // Iniciar el servicio MyService usando la función startService()
-        val intent = Intent(this, MyService::class.java)
-        startService(intent)
-        Toast.makeText(this, "¡El servicio se ha iniciado!", Toast.LENGTH_SHORT).show();
-    }
-
-    val stopServiceButton = findViewById<Button>(R.id.buttonStop)
-    stopServiceButton.setOnClickListener {
-        // Detener el servicio MyService usando la función stopService()
-
-        //val smsManager = SmsManager.getDefault()
-        //smsManager.sendTextMessage("+52" + targetNumber.text.toString(), null, autoResponse.text.toString(), null, null)
-
-        val intent = Intent(this, MyService::class.java)
-        //stopService(intent)
-
-        //Toast.makeText(this, "number: "+targetNumber+", autoResponse: "+autoResponse, Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, targetNumber.text.toString(), Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "---"+autoResponse.text.toString(), Toast.LENGTH_SHORT).show();
-    }
-}
-}
+         */
--- app/src/main/java/com/example/telefonia/MyBroadcastReceiver.kt
+++ app/src/main/java/com/example/telefonia/MyBroadcastReceiver.kt
@@ -5,47 +5,73 @@ import android.app.Activity
 import android.content.*
 import android.content.pm.PackageManager
 import android.os.Build
-import android.os.Bundle
 import android.telephony.SmsManager
 import android.telephony.TelephonyManager
 import android.util.Log
 import android.widget.Toast
-import androidx.annotation.RequiresApi
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import android.widget.EditText
-
 class MyBroadcastReceiver : BroadcastReceiver() {
 
     private lateinit var prefs: SharedPreferences
 
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action == TelephonyManager.ACTION_PHONE_STATE_CHANGED) {
+            val state = intent.getStringExtra(TelephonyManager.EXTRA_STATE)
+            val phoneNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER)
+
+            if (state == TelephonyManager.EXTRA_STATE_RINGING) {
+                if (phoneNumber == getAutoReplyNumber(context)) {
+                    sendAutoReplySms(context)
+                }
+            }
+        }
+    }
+    private fun sendAutoReplySms(context: Context) {
+        val smsManager = SmsManager.getDefault()
+        val autoReplyText = getAutoReplyText(context)
+        val autoReplyNumber = getAutoReplyNumber(context)
+        smsManager.sendTextMessage("+52"+autoReplyNumber, null, autoReplyText, null, null)
+    }
+
+    private fun getAutoReplyNumber(context: Context): String? {
+        val sharedPrefs = context.getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+        return sharedPrefs.getString("targetNumber", null)
+    }
+
+    private fun getAutoReplyText(context: Context): String? {
+        val sharedPrefs = context.getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+        return sharedPrefs.getString("responseMessage", null)
+    }
+
+    /**
     override fun onReceive(context: Context?, intent: Intent?) {
         // Obtener el número de teléfono entrante
         val telephonyManager = context?.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
         val phoneNumber = telephonyManager.line1Number
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (context.checkSelfPermission(Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
-                // Permission is not granted
-                // Request the permission
-                ActivityCompat.requestPermissions(context as Activity, arrayOf(Manifest.permission.SEND_SMS), 1)
-                return
-            }
-        }
 
         // Obtener el número de teléfono de destino y mensaje de respuesta de las preferencias
         prefs = context.getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
         val targetNumber = prefs.getString("targetNumber", "")
         val autoResponse = prefs.getString("autoResponse", "")
 
-        Log.d(", targetNumber", targetNumber.toString())
-        Log.d(", phoneNumber", phoneNumber.toString())
-        Log.d(", mensaje de responder en receiver", autoResponse.toString())
+
+        //val toast = Toast.makeText(targetNumber,autoResponse, Toast.LENGTH_LONG)
+        // toast.show()
+        //Toast.show(this, targetNumber.text.toString(), Toast.LENGTH_SHORT).show();
+
+         Log.d(", targetNumber", targetNumber.toString())
+         Log.d(", phoneNumber", phoneNumber.toString())
+         Log.d(", mensaje de responder en receiver", autoResponse.toString())
 
         // Verificar si el número entrante es el número de destino y enviar un mensaje de respuesta automático
-        if(phoneNumber == targetNumber){
+        //if(phoneNumber!=null && phoneNumber == targetNumber){
+        //    val smsManager = SmsManager.getDefault()
+          //  smsManager.sendTextMessage("+52" + targetNumber, null, autoResponse, null, null)
+        //}
+        //else{
             val smsManager = SmsManager.getDefault()
-            smsManager.sendTextMessage("+52" + targetNumber, null, autoResponse, null, null)
-        }
+            smsManager.sendTextMessage("+524661606569", null, "No entra aqui", null, null)
+    //    }
     }
+    */
 }
