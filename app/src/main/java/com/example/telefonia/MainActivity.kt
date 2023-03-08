8ac1cc643464d23bdc45b29571f38f182f35a99a
GiovanniAdb <78819879+GiovanniAdb@users.noreply.github.com>
2023-03-08 14:10:19 -0600

    Solucion de MainActivity y comprobacion de escucha

--- app/src/main/java/com/example/telefonia/MainActivity.kt
+++ app/src/main/java/com/example/telefonia/MainActivity.kt
@@ -1,117 +1,71 @@
 package com.example.telefonia
-
-import android.Manifest
-import android.content.Context
-import android.content.Intent
-import android.content.pm.PackageManager
+import android.content.*
 import android.os.Bundle
-import android.telephony.PhoneStateListener
-import android.telephony.TelephonyManager
+import android.telephony.SmsManager
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-
 class MainActivity : AppCompatActivity() {
-    private lateinit var phoneInput: EditText
-    private lateinit var replyInput: EditText
-    private lateinit var Savebutton: Button
-    private lateinit var startButton: Button
-    private lateinit var stopButton: Button
-    private var phoneStateListener: PhoneStateListener? = null
+
+    private lateinit var targetNumber: EditText
+    private lateinit var autoResponse: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Obtener los valores guardados en las SharedPreferences
-        val sharedPref =
-            getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE)
-        val savedPhoneNumber = sharedPref.getString(getString(R.string.phone_number_key), null)
-        val savedReplyText = sharedPref.getString(getString(R.string.reply_text_key), null)
+        targetNumber = findViewById(R.id.editTextPhone)
+        autoResponse = findViewById(R.id.editTextMessage)
 
-        // Asignar los valores guardados a las vistas
-        phoneInput.setText(savedPhoneNumber)
-        replyInput.setText(savedReplyText)
+        // Cargar las preferencias guardadas
+        val prefs = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+        val editor = prefs.edit()
+        editor.clear()
+        editor.apply()
+        //targetNumber.setText(prefs.getString("targetNumber", ""))
+        //autoResponse.setText(prefs.getString("autoResponse", ""))
 
-        // Configurar el listener de cambios de estado en llamadas telefónicas
-        val telephonyManager = getSystemService(TELEPHONY_SERVICE) as TelephonyManager
-        val phoneStateListener = object : PhoneStateListener() {
-            override fun onCallStateChanged(state: Int, phoneNumber: String?) {
-                super.onCallStateChanged(state, phoneNumber)
+        Toast.makeText(this, targetNumber.text.toString(), Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "---"+autoResponse.text.toString(), Toast.LENGTH_SHORT).show();
+        Log.d("esto es el mensaje de texto que se manda", autoResponse.toString())
+        //targetNumber.setText("4171400040")
+        //autoResponse.setText("que onda")
 
-                if (state == TelephonyManager.CALL_STATE_RINGING && phoneNumber == savedPhoneNumber) {
-                    // Si el número que está llamando coincide con el número guardado, enviar respuesta automática
-                    val replyIntent = Intent(this@MainActivity, AutoReplyService::class.java)
-                    replyIntent.putExtra("replyText", savedReplyText)
-                    startService(replyIntent)
-                }
-            }
-        }
-
-        // Configurar los botones de la interfaz de usuario
-        Savebutton.setOnClickListener {
-            saveConfig()
-        }
-
-        startButton.setOnClickListener {
-            startService()
-        }
-
-        stopButton.setOnClickListener {
-            stopService()
-        }
+    val saveConfigButton = findViewById<Button>(R.id.buttonSave)
+    saveConfigButton.setOnClickListener {
+        // Obtener el número de teléfono y el mensaje de respuesta automáticos ingresados por el usuario
+        // Guardarlos en las preferencias compartidas usando la clase SharedPreferences
+        val sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putString("targetNumber", targetNumber.text.toString())
+        editor.putString("responseMessage", autoResponse.text.toString())
+        editor.apply()
+        Toast.makeText(this, "¡La configuracion se ha guardado!", Toast.LENGTH_SHORT).show();
     }
 
-    private fun saveConfig() {
-        // Obtener los valores de las vistas
-        val phoneNumber = phoneInput.text.toString()
-        val replyText = replyInput.text.toString()
-
-        // Guardar los valores en las SharedPreferences
-        val sharedPref =
-            getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE)
-        with(sharedPref.edit()) {
-            putString(getString(R.string.phone_number_key), phoneNumber)
-            putString(getString(R.string.reply_text_key), replyText)
-            apply()
-        }
-    }
-
-    private fun startService() {
-        // Configurar el listener de llamadas telefónicas
-        val telephonyManager = getSystemService(TELEPHONY_SERVICE) as TelephonyManager
-        val phoneStateListener = object : PhoneStateListener() {
-            override fun onCallStateChanged(state: Int, phoneNumber: String?) {
-                super.onCallStateChanged(state, phoneNumber)
-
-                if (state == TelephonyManager.CALL_STATE_RINGING && phoneNumber == phoneInput.text.toString()) {
-                    // Si el número que está llamando coincide con el número ingresado, enviar respuesta automática
-                    val replyIntent = Intent(applicationContext, AutoReplyService::class.java)
-                    replyIntent.putExtra("replyText", replyInput.text.toString())
-                    startService(replyIntent)
-                }
-            }
-        }
-        telephonyManager.listen(phoneStateListener, PhoneStateListener.LISTEN_CALL_STATE)
-
-        // Iniciar el servicio de respuesta automática
-        val intent = Intent(this, AutoReplyService::class.java)
+    val startServiceButton = findViewById<Button>(R.id.buttonStart)
+    startServiceButton.setOnClickListener {
+        // Iniciar el servicio MyService usando la función startService()
+        val intent = Intent(this, MyService::class.java)
         startService(intent)
+        Toast.makeText(this, "¡El servicio se ha iniciado!", Toast.LENGTH_SHORT).show();
     }
 
-    private fun stopService() {
-        // Detener el listener de llamadas telefónicas
-        val telephonyManager = getSystemService(TELEPHONY_SERVICE) as TelephonyManager
-        telephonyManager.listen(phoneStateListener, PhoneStateListener.LISTEN_CALL_STATE)
+    val stopServiceButton = findViewById<Button>(R.id.buttonStop)
+    stopServiceButton.setOnClickListener {
+        // Detener el servicio MyService usando la función stopService()
 
-        // Detener el servicio de respuesta automática
-        val intent = Intent(this, AutoReplyService::class.java)
-        stopService(intent)
+        //val smsManager = SmsManager.getDefault()
+        //smsManager.sendTextMessage("+52" + targetNumber.text.toString(), null, autoResponse.text.toString(), null, null)
+
+        val intent = Intent(this, MyService::class.java)
+        //stopService(intent)
+
+        //Toast.makeText(this, "number: "+targetNumber+", autoResponse: "+autoResponse, Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, targetNumber.text.toString(), Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "---"+autoResponse.text.toString(), Toast.LENGTH_SHORT).show();
     }
 }
-
-class AutoReplyService {
-
 }
